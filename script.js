11c543016f5f30534dd3c2d4a4682af4613c8ea0
Deboot <debootdigital@gmail.com>
2026-06-21 09:16:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function mostrarMensaje() {
+    document.getElementById("resultado").innerHTML =
+        "¡Felicitaciones! Tu JavaScript está funcionando.";
+}
+
+function mostrarContacto() {
+    document.getElementById("resultado").innerHTML =
+        "<strong>Contacto</strong><br>Teléfono: +34 123 456 789<br>Email: contacto@ejemplo.com";
+}
+
+function mostrarAnular() {
+    document.getElementById("resultado").innerHTML =
+        "Borrar todo";
+}
